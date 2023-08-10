c5dc5bead243a29c73d400885552fe0df658728d
AutoPR <autopr@irgolic.com>
2023-08-10 05:13:43 +0000

    Implement hello world view

--- testgpt/views.py
+++ testgpt/views.py
@@ -3,12 +3,6 @@ from django.http import HttpResponse
 def hello_world(request):
     return HttpResponse("hello world")
 
-def hello_world(request):
-    return HttpResponse("Hello, World!")
-```
-
-Here is the code enclosed in backticks:
-
 ```
 from django.http import HttpResponse
 
